564a152e4282608c2b7e6a7054be506e742eae24
Pratik Raghubanshi <pratik.raghubanshi@gmail.com>
2021-02-20 12:45:04 +0545

    Addition of clipboard and event detail page

--- wp-content/themes/sparkart/framework-customizations/extensions/shortcodes/shortcodes/events-information/views/view.php
+++ wp-content/themes/sparkart/framework-customizations/extensions/shortcodes/shortcodes/events-information/views/view.php
@@ -3,12 +3,40 @@
 			<div id="content" class="site-content " role="main">
 				<div class="row">
 					<div class="col">
-						<div class="card">
-							<div class="card-body">
-								event information		
+						<div class="card event-detail-card">
+							<div class="event-information-header">
+								<h3>Event Information</h3>
+							</div>
+							<div class="card-body pl-0 pr-0">
+								<div class="row">
+									<div class="col-md-7 col-sm-12">
+										<h2 class="large-event-date">November 17, 2021</h2>
+										<div class="event-detail">
+											<ul>
+												<li><strong>Venue:</strong>Barefoot country Music Festival</li>
+												<li><strong>City:</strong>Wildwood, NJ</li>
+												<li><strong>Country:</strong>United States</li>
+												<li><strong>Directions:</strong><a href="">Google Maps</a></li>
+											</ul>
+											<a class="btn btn-primary" href="javascript:void(0);">Buy Tickets</a>
+										</div>
+									</div>
+									<div class="col-md-5 col-sm-12">
+										<div class="panel-contest">
+											<div class="contest-header text-center">
+												Contest
+											</div>
+											<div class="contest-body">
+												<h4 class="contest-title">CARRIE UNDERWOOD FAN CLUB MEET & GREET</h4>
+												<p>Contest Edns 8/13/21 12:00am CDT</p>
+												<a class="btn btn-outline-secondary">Enter Now</a>
+											</div>
+										</div>
+									</div>
+								</div>	
 								<?php 
 									if ( comments_open() || get_comments_number() ) {
-										echo '<div class="card-comment">';
+										echo '<div class="card-comment-event">';
 										comments_template();
 										echo '</div>';
 
