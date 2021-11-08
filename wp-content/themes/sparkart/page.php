0ccda4fb92205b9f7a3b5e9bfd188b99ef34535f
brandonbews <brandonbews@gmail.com>
2021-11-07 21:04:08 -0800

    About page styling.

--- wp-content/themes/sparkart/page.php
+++ wp-content/themes/sparkart/page.php
@@ -6,8 +6,16 @@ if(have_posts()):
 ?>
 				    	
 <section class="page-section">
-	<div class="container">
-		<h3 class="block-heading text-center my-5"><span><?php the_title(); ?> </span></h3>
+
+	<?php if (has_post_thumbnail()) : ?>
+	<!-- Page header image -->
+	<div class="image-header">
+		<img src="<?php the_post_thumbnail_url("full"); ?>">
+	</div> 
+	<?php endif ?>
+
+	<div class="container container--image-header">
+		<h3 class="block-heading text-center my-5"><span><?php the_title(); ?></span></h3>
 		
 
 		<?php the_content(); ?>
