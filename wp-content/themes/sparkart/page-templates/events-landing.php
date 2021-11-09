1adc3e6dff8fef3ec79d7173286241b320983c75
brandonbews <brandonbews@gmail.com>
2021-11-08 16:45:16 -0800

    Remove video from tour page.

--- wp-content/themes/sparkart/page-templates/events-landing.php
+++ wp-content/themes/sparkart/page-templates/events-landing.php
@@ -11,13 +11,6 @@ if(have_posts()):
 				    	
 <section class="page-section">
 
-	<!-- Video header block -->
-	<div class="video-header">
-				<video playsInline autoPlay loop muted="muted" poster="<?php echo get_template_directory_uri(); ?>/static/videos/tour-sizzle.jpg">
-			<source src="<?php echo get_template_directory_uri(); ?>/static/videos/tour-sizzle.mp4" type="video/mp4"></source>
-		</video>
-	</div> 
-
 	<div class="container container--video-header">
 		<!-- <h3 class="block-heading block-heading--red my-5"><span><?php the_title(); ?> </span></h3> -->
 		
