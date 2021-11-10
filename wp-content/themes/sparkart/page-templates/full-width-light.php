d07da7506f88713d994add24eb9b85e9869df7e2
brandonbews <brandonbews@gmail.com>
2021-11-10 15:39:05 -0800

    Add mobile image and link to header banners.

--- wp-content/themes/sparkart/page-templates/full-width-light.php
+++ wp-content/themes/sparkart/page-templates/full-width-light.php
@@ -9,6 +9,24 @@ get_header();
 
 <div class="full-width--light">
 
+<?php if (has_post_thumbnail()) : ?>
+	<?php
+		$mobile_image = get_field('mobile_featured_image');
+		if ($mobile_image) {
+			$mobile_image_full = $mobile_image['sizes'][ 'large' ];
+		}
+	?>
+	<!-- Page header image -->
+	<div class="image-header">
+			<picture>
+			<?php if ($mobile_image) : 
+				?><source srcset="<?php echo esc_url($mobile_image_full) ?>" media="(max-width: 600px)"/><?php endif; ?>
+				<img src="<?php the_post_thumbnail_url("full"); ?>" alt="<?php ; ?>"/>
+			</picture>
+		<?php if (get_field('featured_image_link')) : ?><a href="<?php the_field('featured_image_link'); ?>" class="image-header__link"></a><?php endif; ?>
+	</div> 
+	<?php endif ?>
+
 <?php
 while(have_posts()):
 	the_post();
--- wp-content/themes/sparkart/page.php
+++ wp-content/themes/sparkart/page.php
@@ -8,11 +8,23 @@ if(have_posts()):
 <section class="page-section">
 
 	<?php if (has_post_thumbnail()) : ?>
+	<?php
+		$mobile_image = get_field('mobile_featured_image');
+		if ($mobile_image) {
+			$mobile_image_full = $mobile_image['sizes'][ 'large' ];
+		}
+	?>
 	<!-- Page header image -->
 	<div class="image-header">
-		<img src="<?php the_post_thumbnail_url("full"); ?>">
+			<picture>
+			<?php if ($mobile_image) : 
+				?><source srcset="<?php echo esc_url($mobile_image_full) ?>" media="(max-width: 600px)"/><?php endif; ?>
+				<img src="<?php the_post_thumbnail_url("full"); ?>" alt="<?php ; ?>"/>
+			</picture>
+		<?php if (get_field('featured_image_link')) : ?><a href="<?php the_field('featured_image_link'); ?>" class="image-header__link"></a><?php endif; ?>
 	</div> 
 	<?php endif ?>
+	
 
 	<div class="container container--image-header">
 		<h3 class="block-heading text-center my-5"><span><?php the_title(); ?></span></h3>
