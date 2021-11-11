2778d422849dd219ea4eebce4ce903865261b8ca
brandonbews <brandonbews@gmail.com>
2021-11-11 08:47:11 -0800

    Fix Mailchimp form collapsing on mobile.

--- wp-content/themes/sparkart/footer.php
+++ wp-content/themes/sparkart/footer.php
@@ -80,41 +80,6 @@
 		});
 	</script>
 
-	<!-- Mailchimp sign up button -->
-	<script>
-		      // Fill in your MailChimp popup settings below.
-      // These can be found in the original popup script from MailChimp.
-      var mailchimpConfig = {
-          baseUrl: 'mc.us17.list-manage.com',
-          uuid: '1ac480ebd6c74965f0d24306c',
-          lid: '7635f918b5'
-      };
-      
-      var chimpPopupLoader = document.createElement("script");
-      chimpPopupLoader.src = '//s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js';
-      chimpPopupLoader.setAttribute('class', 'chimpPopupLoader');
-      chimpPopupLoader.setAttribute('data-dojo-config', 'usePlainJson: true, isDebug: false');
-      jQuery('body').append(chimpPopupLoader);
-
-      var chimpPopup = document.createElement("script");
-      chimpPopup.setAttribute('class', 'chimpPopup');
-      chimpPopup.appendChild(document.createTextNode('require(["mojo/signup-forms/Loader"], function (L) { L.start({"baseUrl": "' +  mailchimpConfig.baseUrl + '", "uuid": "' + mailchimpConfig.uuid + '", "lid": "' + mailchimpConfig.lid + '"})});'));
-
-      document.cookie = "MCPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
-      jQuery(".mailing-list-popup").click(function(e) {
-				console.log('working now')
-        document.cookie.split("; ").forEach(function(c) { if (c.toLowerCase() == "mcpopupclosed=yes") { 
-          jQuery('.chimpPopupLoader').remove();
-          jQuery('.chimpPopup').remove();
-          jQuery('body').append(chimpPopupLoader);
-          document.cookie = "MCPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
-        } });
-
-        jQuery('body').append(chimpPopup);
-      });
-      // End MailChimp popup settings.
-	</script>
-
     <!-- Google Remarketing Pixel -->
     <script type="text/javascript">
     /* <![CDATA[ */
@@ -130,6 +95,7 @@
     <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/979879715/?value=0&amp;guid=ON&amp;script=0"/>
     </div>
     </noscript>
+
 
 </body>
 </html>
